1c13e43bf4379aab7a3053477da5933b239ddbe2
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-11-18 16:22:35 +0000

    exercise 2 - update _config s3 bucket

--- lesson-3-cloud-automation/exercises/starter/exercise-2/_config.tf
+++ lesson-3-cloud-automation/exercises/starter/exercise-2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-tf-emmanuel-student" # Update here with your S3 bucket
+    bucket = "udacity-tf-jaturnereast"
     key    = "terraform/lesson3-ex-2.tfstate"
     region = "us-east-2"
   }
